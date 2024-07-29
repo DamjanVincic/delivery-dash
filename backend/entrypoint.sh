91d89b63fe8aedf8d3198b4b4b864c2123941262
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-29 17:56:59 +0200

    chore: set up superuser creation

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,7 @@
 # Apply migrations
 python manage.py migrate || exit 1
 
+# Create superuser
+python manage.py createsuperuser --noinput || echo "User already exists." # If the user already exists
+
 exec "$@"
