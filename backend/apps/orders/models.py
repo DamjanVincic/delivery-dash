9251778797d3652dcca1aefe5e7d9aa0ce04c9f0
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-30 13:57:12 +0200

    feat: create order model

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Register your models here
+
+class Order(models.Model):
+    CASH = 0
+    CARD = 1
+
+    PENDING = 0
+    DELIVERED = 1
+    FAILED = 2
+
+    PAYMENT_CHOICES = (
+        (CASH, 'Cash'),
+        (CARD, 'Card'),
+    )
+
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (DELIVERED, 'Delivered'),
+        (FAILED, 'Failed'),
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    deliver_at = models.DateTimeField()
+    buyer_firstname = models.CharField(max_length=30)
+    buyer_lastname = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(choices=PAYMENT_CHOICES)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, default=PENDING)
+    # TODO: Add a relationship to the route model
