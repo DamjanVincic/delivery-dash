6d5e06ed5e123b0818fd8d296758e9e7bad14a2d
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 15:54:02 +0200

    feat: add delivery update endpoint

--- backend/apps/deliveries/views/delivery_detail.py
+++ backend/apps/deliveries/views/delivery_detail.py
@@ -3,7 +3,7 @@ from rest_framework import views, status
 from rest_framework.response import Response
 
 from ..models import Delivery
-from ..serializers import DeliverySerializer
+from ..serializers import DeliverySerializer, DeliveryCreateUpdateSerializer
 
 
 class DeliveryDetail(views.APIView):
@@ -14,3 +14,16 @@ class DeliveryDetail(views.APIView):
         except Delivery.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(DeliverySerializer(delivery).data)
+
+    @extend_schema(request=DeliveryCreateUpdateSerializer, responses={200: DeliverySerializer, 404: None, 400: None})
+    def put(self, request, pk):
+        try:
+            delivery = Delivery.objects.get(pk=pk)
+        except Delivery.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = DeliveryCreateUpdateSerializer(delivery, data=request.data)
+        if serializer.is_valid():
+            delivery = serializer.save()
+            serializer = DeliverySerializer(delivery)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
