cae89e49db3ade3bd42d235be2ed98c8ad3aefcb
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 09:48:01 +0200

    feat: add validation to user string fields

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
 
 class User(AbstractUser):
@@ -13,5 +14,7 @@ class User(AbstractUser):
         (DISPATCHER, 'Dispatcher'),
     )
 
+    first_name = models.CharField(max_length=30, validators=[MinLengthValidator(1)])
+    last_name = models.CharField(max_length=30, validators=[MinLengthValidator(1)])
     role = models.SmallIntegerField(choices=ROLE_CHOICES, null=True)  # Temp for superuser creation
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, validators=[MinLengthValidator(10)])
