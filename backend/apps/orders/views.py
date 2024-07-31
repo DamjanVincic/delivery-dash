3c7ff1ada6aab7228dc00770c8bffae95fb07dad
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 13:54:04 +0200

    feat: add order post method

--- backend/apps/orders/views.py
+++ backend/apps/orders/views.py
@@ -3,6 +3,7 @@ from .serializers import OrderSerializer
 from rest_framework import views
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
 
 
 class OrderList(views.APIView):
@@ -11,3 +12,11 @@ class OrderList(views.APIView):
         orders = Order.objects.all()
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
+
+    @extend_schema(request=OrderSerializer, responses={201: OrderSerializer, 400: None})
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
