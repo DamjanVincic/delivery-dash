588a36d2890600d5f4ed7fa6f0f15c9be0953154
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 15:48:22 +0200

    feat: add delivery post endpoint

--- backend/apps/deliveries/views/delivery_list.py
+++ backend/apps/deliveries/views/delivery_list.py
@@ -1,7 +1,8 @@
 from ..models import Delivery
-from ..serializers import DeliverySerializer
+from ..serializers import DeliverySerializer, DeliveryCreateUpdateSerializer
 from rest_framework import views
 from rest_framework.response import Response
+from rest_framework import status
 from drf_spectacular.utils import extend_schema
 
 
@@ -11,3 +12,12 @@ class DeliveryList(views.APIView):
         deliveries = Delivery.objects.all()
         serializer = DeliverySerializer(deliveries, many=True)
         return Response(serializer.data)
+
+    @extend_schema(request=DeliveryCreateUpdateSerializer, responses={201: DeliverySerializer, 400: None})
+    def post(self, request):
+        serializer = DeliveryCreateUpdateSerializer(data=request.data)
+        if serializer.is_valid():
+            delivery = serializer.save()
+            serializer = DeliverySerializer(delivery)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
