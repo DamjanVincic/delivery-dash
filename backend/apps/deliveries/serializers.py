128de5114a595995d38a299eed8db78a9f3526dc
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 09:51:46 +0200

    feat: add delivery serializer

---
+++ backend/apps/deliveries/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Delivery
+from apps.users.serializers import UserSerializer
+
+
+class DeliverySerializer(serializers.ModelSerializer):
+    driver = UserSerializer()
+
+    class Meta:
+        model = Delivery
+        fields = ['driver']
