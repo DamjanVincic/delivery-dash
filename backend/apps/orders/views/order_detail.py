bd2741cf187c55c43f21536860fcca699880bc40
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 14:25:26 +0200

    feat: add swagger annotations to order detail endpoints

--- backend/apps/orders/views/order_detail.py
+++ backend/apps/orders/views/order_detail.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 class OrderDetail(views.APIView):
+    @extend_schema(responses={200: OrderSerializer, 404: None})
     def get(self, request, pk):
         try:
             order = Order.objects.get(pk=pk)
@@ -15,6 +16,7 @@ class OrderDetail(views.APIView):
         serializer = OrderSerializer(order)
         return Response(serializer.data)
 
+    @extend_schema(request=OrderSerializer, responses={200: OrderSerializer, 400: None, 404: None})
     def put(self, request, pk):
         try:
             order = Order.objects.get(pk=pk)
@@ -26,10 +28,11 @@ class OrderDetail(views.APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(request=OrderSerializer, responses={204: None})
     def delete(self, request, pk):
         try:
             order = Order.objects.get(pk=pk)
+            order.delete()
         except Order.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        order.delete()
+            pass
         return Response(status=status.HTTP_204_NO_CONTENT)
