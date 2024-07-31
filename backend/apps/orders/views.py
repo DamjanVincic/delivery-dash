0a0deea25fd9e9a14c9411cc6097205896574fbd
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-31 09:54:19 +0200

    feat: create order list view

--- backend/apps/orders/views.py
+++ backend/apps/orders/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Order
+from .serializers import OrderSerializer
+from rest_framework import views
+from rest_framework.response import Response
 
-# Create your views here.
+
+class OrderList(views.APIView):
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
