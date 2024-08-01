64ef9bab4583cf98ed7d8c8260b8e952cdd897e2
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-01 16:36:03 +0200

    feat: create dispatcher deliver list view

--- backend/apps/deliveries/views/__init__.py
+++ backend/apps/deliveries/views/__init__.py
@@ -1,3 +1,4 @@
 from .delivery_list import DeliveryList
 from .delivery_detail import DeliveryDetail
 from .driver_delivery_list import DriverDeliveryList
+from .dispatcher_delivery_list import DispatcherDeliveryList
