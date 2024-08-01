f8399c7e6379fa2d775519ca151840481e375b45
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-01 14:34:54 +0200

    feat: add delivery field to user serializer

--- backend/apps/users/serializers/user_serializer.py
+++ backend/apps/users/serializers/user_serializer.py
@@ -8,5 +8,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'role', 'phone_number']
-        read_only_fields = ['id']
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'role', 'phone_number', 'delivery']
+        read_only_fields = ['id', 'delivery']
