cc0b7d0614a9be8d64b0d4cb0ead9bc2b0648693
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-01 14:58:30 +0200

    feat: add new order fields to serializer

--- backend/apps/orders/serializers.py
+++ backend/apps/orders/serializers.py
@@ -4,8 +4,17 @@ from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    is_late = serializers.SerializerMethodField(read_only=True)
+    late_time = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Order
         fields = ['id', 'created_at', 'deliver_at', 'buyer_firstname', 'buyer_lastname', 'address', 'phone_number',
-                  'price', 'payment_method', 'status', 'delivery']
-        read_only_fields = ['id', 'created_at', 'status', 'delivery']
+                  'price', 'payment_method', 'status', 'delivery', 'comment', 'is_late', 'late_time']
+        read_only_fields = ['id', 'created_at', 'status', 'delivery', 'comment']
+
+    def get_is_late(self, obj) -> serializers.BooleanField:
+        return obj.delivered_at is not None and obj.delivered_at > obj.deliver_at
+
+    def get_late_time(self, obj):
+        return (obj.delivered_at - obj.deliver_at).seconds // 60 if obj.delivered_at is not None else None
