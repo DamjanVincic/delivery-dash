0052945505e803b6bd1591aba125eeae22c5ebee
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-09 16:09:29 +0200

    feat: implement routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
 import NavBar from "./components/NavBar.jsx";
 import LoginPage from "./views/LoginPage.jsx";
+import DispatcherPage from "./views/DispatcherPage.jsx";
+import DriverPage from "./views/DriverPage.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
-      <LoginPage />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<LoginPage />} />
+          <Route path="/dispatcher" element={<DispatcherPage />} />
+          <Route path="/driver" element={<DriverPage />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   );
