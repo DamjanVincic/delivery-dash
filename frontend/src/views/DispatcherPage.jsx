c8ec753fe869d7822c59805fd2c47a09121c2a39
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-09 14:56:32 +0200

    feat: add a list of deliveries to dispatcher page

--- frontend/src/views/DispatcherPage.jsx
+++ frontend/src/views/DispatcherPage.jsx
@@ -7,6 +7,7 @@ import {
 } from "mdb-react-ui-kit";
 import Drivers from "../components/dispatcher/Drivers";
 import Orders from "../components/dispatcher/Orders";
+import Deliveries from "../components/dispatcher/Deliveries";
 
 const orders = [
   {
@@ -48,6 +49,51 @@ const drivers = [
   },
 ];
 
+const deliveries = [
+  {
+    id: 1,
+    driver_id: 1,
+    order_id: 1,
+    orders: [
+      {
+        id: 1,
+        buyer_firstname: "John",
+        buyer_lastname: "Doe",
+        address: "123 Elm St",
+        status: "Pending",
+        payment_method: "cash",
+        price: 100,
+      },
+      {
+        id: 2,
+        buyer_firstname: "Jane",
+        buyer_lastname: "Smith",
+        address: "456 Oak Ave",
+        status: "Failed",
+        payment_method: "card",
+        price: 50,
+      },
+      {
+        id: 3,
+        buyer_firstname: "Alice",
+        buyer_lastname: "Johnson",
+        address: "789 Pine Rd",
+        status: "Delivered",
+        payment_method: "cash",
+        price: 75,
+      },
+    ],
+    status: "Pending",
+  },
+  {
+    id: 2,
+    driver_id: 2,
+    order_id: 2,
+    orders: [],
+    status: "Pending",
+  },
+];
+
 export default function DispatcherPage() {
   return (
     <MDBContainer>
@@ -60,14 +106,8 @@ export default function DispatcherPage() {
         </MDBCol>
       </MDBRow>
       <MDBRow center>
-        <MDBCol size="4">
-          <MDBTable>
-            <MDBTableHead>
-              <tr>
-                <th>Test</th>
-              </tr>
-            </MDBTableHead>
-          </MDBTable>
+        <MDBCol size="12">
+          <Deliveries deliveries={deliveries} />
         </MDBCol>
       </MDBRow>
     </MDBContainer>
