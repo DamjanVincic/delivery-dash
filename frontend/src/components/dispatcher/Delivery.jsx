7b216d4e91a2d7c9dedf04fcb26b401296f15389
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-09 14:55:50 +0200

    feat: create delivery component for dispatcher page

---
+++ frontend/src/components/dispatcher/Delivery.jsx
@@ -0,0 +1,54 @@
+import {
+  MDBBtn,
+  MDBBadge,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+import Order from "./Order";
+import { getDeliveryStatusBadgeColor } from "../../utils/common";
+
+export default function Delivery({ delivery, showOrders, setShowOrders }) {
+  const { orders, status } = delivery;
+
+  return (
+    <>
+      <tr>
+        <td>Driver</td>
+        <td>
+          <MDBBadge color={getDeliveryStatusBadgeColor(status)} pill>
+            {status}
+          </MDBBadge>
+        </td>
+        <td>
+          <MDBBtn onClick={() => setShowOrders(delivery.id)}>
+            {showOrders === delivery.id ? "Hide" : "Show"} Orders
+          </MDBBtn>
+        </td>
+      </tr>
+      <tr>
+        <td colSpan={3}>
+          <MDBCollapse open={showOrders === delivery.id}>
+            <MDBTable responsive striped hover>
+              <MDBTableHead>
+                <tr>
+                  <th scope="col">Customer</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Details</th>
+                </tr>
+              </MDBTableHead>
+              <MDBTableBody>
+                {orders.map((order) => (
+                  <Order key={order.id} order={order} />
+                ))}
+              </MDBTableBody>
+            </MDBTable>
+          </MDBCollapse>
+        </td>
+      </tr>
+    </>
+  );
+}
