62d9e600e3282e5218229cd0949dbb240bbb3f4d
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-13 13:59:09 +0200

    feat(frontend): implement destructuring in order details modal

--- frontend/src/components/OrderDetailModal.jsx
+++ frontend/src/components/OrderDetailModal.jsx
@@ -19,12 +19,27 @@ import {
 } from "../utils/common";
 
 export default function OrderDetailModal({ show, onClose, order }) {
+  const {
+    id,
+    buyer_firstname,
+    buyer_lastname,
+    deliver_at,
+    address,
+    phone_number,
+    price,
+    payment_method,
+    status,
+    comment,
+    delivered_at,
+    late_time,
+  } = order;
+
   return (
     <MDBModal open={show} onClose={onClose} tabIndex="-1">
       <MDBModalDialog>
         <MDBModalContent>
           <MDBModalHeader>
-            <MDBModalTitle>Order #{order.id}</MDBModalTitle>
+            <MDBModalTitle>Order #{id}</MDBModalTitle>
             <MDBBtn className="btn-close" color="none" onClick={onClose} />
           </MDBModalHeader>
           <MDBModalBody>
@@ -38,48 +53,45 @@ export default function OrderDetailModal({ show, onClose, order }) {
                   <td>
                     <strong>Customer Name</strong>
                   </td>
-                  <td>{order.buyer_firstname + " " + order.buyer_lastname}</td>
+                  <td>{buyer_firstname + " " + buyer_lastname}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Deliver At</strong>
                   </td>
-                  <td>{parseDateTime(order.deliver_at)}</td>
+                  <td>{parseDateTime(deliver_at)}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Address</strong>
                   </td>
-                  <td>{order.address}</td>
+                  <td>{address}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Phone Number</strong>
                   </td>
-                  <td>{order.phone_number}</td>
+                  <td>{phone_number}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Price</strong>
                   </td>
-                  <td>{order.price}</td>
+                  <td>{price}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Payment Method</strong>
                   </td>
-                  <td>{order.payment_method}</td>
+                  <td>{payment_method}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Status</strong>
                   </td>
                   <td>
-                    <MDBBadge
-                      color={getOrderStatusBadgeColor(order.status)}
-                      pill
-                    >
-                      {formatString(order.status)}
+                    <MDBBadge color={getOrderStatusBadgeColor(status)} pill>
+                      {formatString(status)}
                     </MDBBadge>
                   </td>
                 </tr>
@@ -87,25 +99,19 @@ export default function OrderDetailModal({ show, onClose, order }) {
                   <td>
                     <strong>Comment</strong>
                   </td>
-                  <td>{order.comment ? order.comment : "/"}</td>
+                  <td>{comment ? comment : "/"}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Delivered At</strong>
                   </td>
-                  <td>
-                    {order.delivered_at
-                      ? parseDateTime(order.delivered_at)
-                      : "/"}
-                  </td>
+                  <td>{delivered_at ? parseDateTime(delivered_at) : "/"}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Late Time</strong>
                   </td>
-                  <td>
-                    {order.late_time ? `${order.late_time} minutes` : "/"}
-                  </td>
+                  <td>{late_time ? `${late_time} minutes` : "/"}</td>
                 </tr>
               </MDBTableBody>
             </MDBTable>
