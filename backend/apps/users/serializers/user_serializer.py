f0937b054ec65e7b4bfe3927840991e1e667b132
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-13 12:23:06 +0200

    feat(backend): update user serializer to reflect the model cardinality change

--- backend/apps/users/serializers/user_serializer.py
+++ backend/apps/users/serializers/user_serializer.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from apps.deliveries.models import Delivery
+
 from ..models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    delivery = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
@@ -19,4 +22,9 @@ class UserSerializer(serializers.ModelSerializer):
             'phone_number',
             'delivery',
         ]
-        read_only_fields = ['id', 'delivery']
+        read_only_fields = ['id']
+
+    def get_delivery(self, obj):
+        if delivery := obj.deliveries.filter(status=Delivery.IN_PROGRESS).first():
+            return delivery.pk
+        return None
