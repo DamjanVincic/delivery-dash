8f856e812cf5a357f85f0b311b53a31b9ebeef5a
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-13 16:07:19 +0200

    feat(backend): update order representation in django admin

--- backend/apps/orders/admin.py
+++ backend/apps/orders/admin.py
@@ -4,7 +4,25 @@ from .models import Order
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_filter = ['status', 'delivery']
+    list_display = [
+        'details',
+        'customer',
+        'deliver_at',
+        'delivered_at',
+        'address',
+        'phone_number',
+        'price',
+        'payment_method',
+        'status',
+        'delivery',
+    ]
+    list_filter = ['status', 'delivery', 'payment_method']
+
+    def details(self, obj):
+        return f'Order #{obj.id}'
+
+    def customer(self, obj):
+        return f'{obj.buyer_firstname} {obj.buyer_lastname}'
 
 
 admin.site.register(Order, OrderAdmin)
