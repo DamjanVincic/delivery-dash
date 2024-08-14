02695d2474a7747d516bde3781915b1f919fdbdb
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-14 11:06:50 +0200

    feat(backend): update delivery representation in django admin table

--- backend/apps/deliveries/admin.py
+++ backend/apps/deliveries/admin.py
@@ -12,11 +12,16 @@ class OrderInLine(admin.StackedInline):
 
 class DeliveryAdmin(admin.ModelAdmin):
     inlines = [OrderInLine]
+    list_display = ['delivery', 'driver', 'status']
+    list_filter = ['driver', 'status']
 
     def get_form(self, request, obj=None, **kwargs):
         if obj:
             return DeliveryChangeForm
         return DeliveryAddForm
+
+    def delivery(self, obj):
+        return f'Delivery #{obj.pk}'
 
 
 admin.site.register(Delivery, DeliveryAdmin)
