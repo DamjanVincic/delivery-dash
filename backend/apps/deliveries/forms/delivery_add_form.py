1524557685deb338738e7831cddce80d2307691f
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-14 12:48:00 +0200

    feat(backend): allow only drivers to be chosen when creating deliveries

--- backend/apps/deliveries/forms/delivery_add_form.py
+++ backend/apps/deliveries/forms/delivery_add_form.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from apps.orders.models import Order
+from apps.users.models import User
 
 from ..models import Delivery
 
@@ -9,6 +10,10 @@ class DeliveryAddForm(forms.ModelForm):
     class Meta:
         model = Delivery
         fields = ['driver', 'orders']
+
+    driver = forms.ModelChoiceField(
+        queryset=User.objects.filter(role=User.DRIVER), required=False
+    )
 
     orders = forms.ModelMultipleChoiceField(
         queryset=Order.objects.filter(delivery=None),
@@ -20,5 +25,4 @@ class DeliveryAddForm(forms.ModelForm):
         delivery = super().save(commit=False)
         delivery.save()
         self.cleaned_data['orders'].update(delivery=delivery)
-
         return delivery
