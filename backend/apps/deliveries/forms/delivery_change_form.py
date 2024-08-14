83fb0ea90cf14d1cd84b2bc831a09b8271e75d0c
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-14 13:03:11 +0200

    feat(backend): update delivery change form to allow for easier checking of orders

--- backend/apps/deliveries/forms/delivery_change_form.py
+++ backend/apps/deliveries/forms/delivery_change_form.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.db.models import Q
+
+from apps.orders.models import Order
+from apps.users.models import User
 
 from ..models import Delivery
 
@@ -6,4 +10,27 @@ from ..models import Delivery
 class DeliveryChangeForm(forms.ModelForm):
     class Meta:
         model = Delivery
-        fields = ['driver', 'status']
+        fields = ['driver', 'status', 'orders']
+
+    driver = forms.ModelChoiceField(
+        queryset=User.objects.filter(role=User.DRIVER), required=False
+    )
+
+    orders = forms.ModelMultipleChoiceField(
+        queryset=Order.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['orders'].queryset = Order.objects.filter(
+            Q(delivery=self.instance) | Q(delivery=None)
+        )
+        self.fields['orders'].initial = self.instance.orders.all()
+
+    def save(self, *args, **kwargs):
+        delivery = super().save(commit=False)
+        delivery.save()
+        self.fields['orders'].initial.update(delivery=None)
+        self.cleaned_data['orders'].update(delivery=delivery)
+        return delivery
