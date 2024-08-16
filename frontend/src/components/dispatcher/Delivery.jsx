125849c32b4836b76ce0ea119a662d6e5be0ff65
DamjanVincic <damjanvincic2003@gmail.com>
2024-08-16 11:00:34 +0200

    feat(frontend): add missing column name to delivery in dispatcher page

--- frontend/src/components/dispatcher/Delivery.jsx
+++ frontend/src/components/dispatcher/Delivery.jsx
@@ -37,6 +37,7 @@ export default function Delivery({ delivery, showOrders, setShowOrders }) {
                   <th scope="col">Address</th>
                   <th scope="col">Price</th>
                   <th scope="col">Status</th>
+                  <th scope="col">Late Time</th>
                   <th scope="col">Details</th>
                 </tr>
               </MDBTableHead>
